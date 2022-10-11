78b9a125e7fec1ff43a6e0f78452f0ea8f5cf246
Mweh <fahmianton@gmail.com>
2022-10-11 13:13:17 +0800

    v.1

--- assignment1 profileApp mes/ViewController.swift
+++ assignment1 profileApp mes/ViewController.swift
@@ -5,13 +5,15 @@
 //  Created by Muhammad Fahmi on 11/10/22.
 //
 
+// lupa ilangin spacing di project's namenya, harusnya camelCase
+// singkat aja utk pengubahan pd IBActionnya dgn alpha value dr gambar
+
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var hobbyLabel: UILabel!
-    // pizzaImageView set to default Image = grey background Image
     @IBOutlet weak var pizzaImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -21,7 +23,9 @@ class ViewController: UIViewController {
     
     @IBAction func generatePizzaButton(_ sender: UIButton) {
         // #imageLiteral(resourceName: "pizza.jpeg")
-//        pizzaImageView.image = #imageLiteral(resourceName: "pizza")
+        // pizzaImageView.image = #imageLiteral(resourceName: "pizza")
+        // pizzaImageView-alpha's default value = 0
+
         pizzaImageView.alpha = 1
     }
 }
